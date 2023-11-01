3f790904f622ffe37368bcbdc4796d74a2b771ca
Nikson Diniz <nikson.diniz@gmail.com>
2023-11-01 20:02:00 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/ndiniz/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ndiniz.workshopmongo.repository;
+
+import com.ndiniz.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
