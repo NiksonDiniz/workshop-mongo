256a2874d53fc0bdc6b712bd1d03d4006cfa7bad
Nikson Diniz <nikson.diniz@gmail.com>
2023-11-02 10:30:41 -0300

    Obtendo um usuário por id

--- src/main/java/com/ndiniz/workshopmongo/services/UserService.java
+++ src/main/java/com/ndiniz/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.ndiniz.workshopmongo.services;
 
 import com.ndiniz.workshopmongo.domain.User;
 import com.ndiniz.workshopmongo.repository.UserRepository;
+import com.ndiniz.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
    public List<User> findAll() {
       return repo.findAll();
+   }
+
+   public User findById(String id) {
+      Optional<User> obj = repo.findById(id);
+      return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
    }
 }
---
+++ src/main/java/com/ndiniz/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.ndiniz.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+   public ObjectNotFoundException(String msg) {
+      super(msg);
+   }
+}
