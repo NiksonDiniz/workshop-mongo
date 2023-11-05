706789030dcd89bcab0d5eb194754229902c7b7b
Nikson Diniz <nikson.diniz@gmail.com>
2023-11-05 10:06:11 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/ndiniz/workshopmongo/config/Instantiation.java
+++ src/main/java/com/ndiniz/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.ndiniz.workshopmongo.config;
 import com.ndiniz.workshopmongo.domain.Post;
 import com.ndiniz.workshopmongo.domain.User;
 import com.ndiniz.workshopmongo.dto.AuthorDTO;
+import com.ndiniz.workshopmongo.dto.CommentDTO;
 import com.ndiniz.workshopmongo.repository.PostRepository;
 import com.ndiniz.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.List;
 import java.util.TimeZone;
 
 @Configuration
@@ -43,6 +45,13 @@ public class Instantiation implements CommandLineRunner {
               "Bom dia",
               "Acordei feliz hoje!!",
               new AuthorDTO(maria));
+
+      CommentDTO c1 = new CommentDTO("Boa viagem mano!!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+      CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+      CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+      post1.getComments().addAll(Arrays.asList(c1, c2));
+      post2.getComments().add(c3);
 
       postRepository.saveAll(Arrays.asList(post1, post2));
 
