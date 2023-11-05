b7d272e6d12df0535f71ef0254afdde7801c7675
Nikson Diniz <nikson.diniz@gmail.com>
2023-11-05 09:50:11 -0300

    Obtendo um post por id

---
+++ src/main/java/com/ndiniz/workshopmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.ndiniz.workshopmongo.services;
+
+import com.ndiniz.workshopmongo.domain.Post;
+import com.ndiniz.workshopmongo.domain.User;
+import com.ndiniz.workshopmongo.dto.UserDTO;
+import com.ndiniz.workshopmongo.repository.PostRepository;
+import com.ndiniz.workshopmongo.repository.UserRepository;
+import com.ndiniz.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+   @Autowired
+   private PostRepository repo;
+
+   public Post findById(String id) {
+      Optional<Post> obj = repo.findById(id);
+      return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+   }
+
+}
